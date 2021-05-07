83c43f246c855ea39faf501eff4c55f0c17e425c
nullx8 <mario@hennenberger.de>
2021-05-07 16:30:20 +0700

    hide metrics link

--- swingfish/swingfish.py
+++ swingfish/swingfish.py
@@ -566,6 +566,12 @@ class SwingFish(BaseCog):
                                   f"Status: {enfdata['propstatus']} [[Metrics]({enfdata['proplink']})]",
                             inline=True
                         )
+                    else:
+                        data.add_field(
+                            name="Prop Trader",
+                            value=f"Status: {enfdata['propstatus']}",
+                            inline=True
+                        )
         #                    data.add_field(name="Lender Gain", value=str(enfdata['gain'])+"%", inline=True)
         #                else:
         #                    role = discord.utils.get(user.server.roles, name="EnFoid Prop-Trader")
