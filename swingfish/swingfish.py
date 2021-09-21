b75dbf95c580ab7cac395ec4474c00603b5a2aab
nullx8 <mario@hennenberger.de>
2021-09-21 20:43:21 +0700

    Testing stats

--- swingfish/swingfish.py
+++ swingfish/swingfish.py
@@ -80,6 +80,19 @@ class SwingFish(BaseCog):
         with open(f'/var/www/swingfish.trade/html/assets/cache/discord_members_{role_id}.json', 'w') as f:
             json.dump(data, f)
 
+	@commands.command(pass_context=True)
+	async def stat(ctx):
+    	"""Returns a CSV file of all users on the server."""
+		await bot.request_offline_members(ctx.message.server)
+		before = time.time()
+		nicknames = [m.display_name for m in ctx.message.server.members]
+		with open('temp.csv', mode='w', encoding='utf-8', newline='') as f:
+        	writer = csv.writer(f, dialect='excel')
+			for v in nicknames:
+            	writer.writerow([v])
+			after = time.time()
+		await bot.send_file(ctx.message.author, 'temp.csv', filename='stats.csv', content="Here you go! Check your PM's. Generated in {:.4}ms.".format((after - before)*1000))
+                        
     @commands.command()
     async def help(self, ctx: Context):
         """Need Help .. huh ?"""
